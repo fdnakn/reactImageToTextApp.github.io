d5e41b43a083add09caf3c0abcc92f5c262c2dca
akin <akinfidancal@hotmail.com>
2022-07-24 14:42:30 +0300

    imagetotext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React , {useEffect, useState} from 'react';
+import { createWorker } from 'tesseract.js';
 import './App.css';
 
 function App() {
+  const [selectedImage,setSelectedImage] = useState(null);
+  const [textResult,setTextResult] = useState("");
+
+  const worker = createWorker();
+
+  const convertImageToText = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data } = await worker.recognize(selectedImage);
+    console.log(data);
+    await worker.terminate();
+    setTextResult(data.text);
+  }
+  useEffect(()=>{
+    convertImageToText();
+  },[selectedImage])
+
+  const handleChangeImage = e =>{
+    setSelectedImage(e.target.files[0])  
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image to Text App</h1>
+      <p>Get words in image !</p>
+      <div className='input-wrapper'>
+        <label htmlFor='upload'>Upload Image</label>
+        <input type="file" id='upload' accept='image/*' onChange={handleChangeImage} />
+      </div>
+
+      <div className='result'>
+      {selectedImage && (
+        <img src={URL.createObjectURL(selectedImage)} alt="thumbnail" />
+      )}
+      {textResult && (
+        <div className='box-p'>
+          <p>{textResult}</p>
+        </div>
+      )}
+      </div>
     </div>
   );
 }
